7bfa5873fb85a3e8a1bf73a5104a2ade49ba530c
Adebola Fagbowore <bfagbowore@gmail.com>
2021-01-17 10:22:08 +0100

    students page update

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -31,8 +31,8 @@
                                         <td>{{$user->name}}</td>
                                         <td>{{$user->level}}</td>
                                         <td>{{$user->matric_no}}</td>
-                                        <td>{{$user->faculties->name}}</td>
-                                        <td>{{$user->departments->name}}</td>
+                                        <td>{{optional($user->faculties)->name}}</td>
+                                        <td>{{optional($user->departments)->name}}</td>
                                         <td>{{$user->programme}}</td>
                                         <td>{{$user->gender}}</td>
                                         <td><button class="btn btn-info" data-toggle="modal" data-target="#student{{$user->id}}"><i class="fa fa-edit"></i></button></td>
